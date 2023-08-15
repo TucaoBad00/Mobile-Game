6689d79c999a27d75ac47be4c775c8e47d4ea3f1
TucaoBad00 <arthurbadotti@hotmail.com>
2023-08-15 18:53:33 -0300

    Leveis aleatórios

--- Assets/Scripts/ETC/LevelManager.cs
+++ Assets/Scripts/ETC/LevelManager.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     public Transform levelContainer;
+
     public List<GameObject> level;
+
     public int _index;
+
     private GameObject currentLevel;
+
+    public List<LevelPathBase> LevelPaths;
+    private List<LevelPathBase> SpawnedPaths;
+    public int PathsNumber;
     private void Awake()
     {
-        SpawNextLevel();
+        //SpawNextLevel();
+        CreateLevelPaths();
     }
     public void SpawNextLevel()
     {
@@ -23,13 +31,37 @@ public class LevelManager : MonoBehaviour
         }
         if (_index >= level.Count)
         {
-            _index = 0;
+            ResetLevel();
         }
         currentLevel = Instantiate(level[_index], levelContainer);
         currentLevel.transform.localPosition = Vector3.zero;
     }
     private void ResetLevel()
     {
+        _index = 0;
+    }
 
+
+    private void CreateLevelPath()
+    {
+        var path = LevelPaths[Random.Range(0, LevelPaths.Count)];
+        var spawnedPath = Instantiate(path, levelContainer);
+        if(SpawnedPaths.Count > 0)
+        {
+            var lastPath = SpawnedPaths[SpawnedPaths.Count - 1];
+            spawnedPath.transform.position = lastPath.endPath.position;
+        }
+        SpawnedPaths.Add(spawnedPath);
+    }
+
+    private void CreateLevelPaths()
+    {
+        SpawnedPaths = new List<LevelPathBase>();
+
+        for(int i = 0; i < PathsNumber; i++)
+        {
+            CreateLevelPath();
+        }
     }
 }
+    
